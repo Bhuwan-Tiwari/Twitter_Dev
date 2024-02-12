783e52d809845623750844f48d7c3de9f04c0d2e
Bhuwan Tiwari <bhuwan1200@gmail.com>
2024-02-12 17:24:36 +0530

    Added codes

---
+++ src/repository/tweet-repository.js
@@ -0,0 +1,54 @@
+const Tweet = require('../models/tweet')
+
+
+class TweetRepository
+{
+    async create(data)
+    {
+        try {
+            const tweet = await Tweet.create(data)
+            return tweet
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async get(id)
+    {
+        try {
+            const tweet = await Tweet.findById(id)
+            return tweet
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async update(id,data)
+    {
+        try {
+            const tweet = await Tweet.findByIdAndUpdate(id,data)
+            return tweet
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async destroy(id)
+    {
+        try {
+            const tweet = await Tweet.findByIdAndDelete(id)
+            return tweet
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async getwithComments(id)
+    {
+        try {
+            const tweet = await Tweet.findById(id).populate({path:'comments'})  //comments attribute of model
+            return tweet
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+module.exports=TweetRepository

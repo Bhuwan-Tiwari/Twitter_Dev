5099c2b0b5c5f3032992784890626206dc5c4943
Bhuwan Tiwari <bhuwan1200@gmail.com>
2024-02-18 22:55:55 +0530

    updated comment model with refpath

--- src/models/comment.js
+++ src/models/comment.js
@@ -3,13 +3,25 @@ const mongoose = require('mongoose')
 const commentschema = new mongoose.Schema({
     content: {
         type: String,
-        required :true
+        required: true
     },
-    userEmail: {
-        type: String
+    onModel: {
+        type: String,
+        require: true,
+        enum: ['Tweet', 'comment']   //if we have to reffer more than on model we use enum other than ref
+    },
+    commentable: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: true,
+        refPath: 'onModel' //ref to model id    
+        //it ref model id to which model it belongs              //refers the value
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',   //reffer to userid
+        required: true
     }
-    
-},{timestamps: true})
+}, { timestamps: true })
 
 const Comment = mongoose.model("Comment", commentschema)
 module.exports = Comment
